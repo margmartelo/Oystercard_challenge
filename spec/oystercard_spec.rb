1e9e3912e1e8e6bf2b5c721d2a2e83ca092cd8f8
Margarida Martelo <margmartelo@gmail.com>
2021-07-18 16:56:20 +0100

    Refactoring

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,15 +5,16 @@ describe Oystercard do
       expect(subject.balance).to eq 0
     end
 
-    it "can be topped up by a chosen amount of money" do 
-      subject.top_up(10)
-      expect(subject.balance).to eq 10
-    end
+    describe "#top_up" do
+      it "can be topped up by a chosen amount of money" do 
+        subject.top_up(10)
+        expect(subject.balance).to eq 10
+      end
 
-    it "will raise an error if balance passes its maximum value" do
-      maximum_balance = Oystercard::MAXIMUM_BALANCE
-      expect { subject.top_up(95) }.to raise_error("Your card's balance can't exceed £#{maximum_balance}. Your current balance is £#{subject.balance}.")
+      it "will raise an error if balance passes the maximum value allowed" do
+        maximum_balance = Oystercard::MAXIMUM_BALANCE
+        expect { subject.top_up(95) }.to raise_error("Your card's balance can't exceed £#{maximum_balance}. Your current balance is £#{subject.balance}.")
+      end
     end
-     
 
 end
