46f5b90b13b247c60b4117299ee588b97dfd616c
Margarida Martelo <margmartelo@gmail.com>
2021-07-18 15:21:28 +0100

    Added test for top up functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,9 @@ describe Oystercard do
     it "should have an initial default balance of 0" do
       expect(subject.balance).to eq 0
     end
+
+    it "can be topped up by a chosen amount of money" do 
+      subject.top_up(10)
+      expect(subject.balance).to eq 10
+    end
 end
