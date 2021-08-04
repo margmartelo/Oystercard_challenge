aeff22a7ab49f2260968abc9d433434be4c98718
Margarida Martelo <margmartelo@gmail.com>
2021-08-04 16:37:41 +0200

    Refactoring: creating doubles across journey and oystercard spec files

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,55 +1,54 @@
 require 'journey'
 
 describe Journey do
+  let(:entry_station) { double :station }
+  let(:exit_station) { double :station }
 
-    it "knows if a journey is not complete" do
-      expect(subject.complete?).to be false
-    end
+  it "knows if a journey is not complete" do
+    expect(subject.complete?).to be false
+  end
 
-    it "knows if a journey is complete" do
-      @journey = Journey.new("Liverpool Street")
-      @journey.finish("Walthamstow Central")
-      expect(@journey.complete?).to be true
+  context 'if not given an entry station' do
+    it "will default to a nil entry station" do
+        expect(subject.entry_station).to eq nil
     end
+  end
 
-    it "returns itself when exiting a journey" do
-      @journey = Journey.new("Liverpool Street")
-      expect(@journey.finish("Walthamstow Central")).to eq @journey
-    end
-
-    context 'if not given an entry station' do
-      it "will default to a nil entry station" do
-          expect(subject.entry_station).to eq nil
-      end
-    end
+  before do 
+    @journey = Journey.new(entry_station)
+  end        
 
     context 'given an entry station' do
-      before do 
-          @journey = Journey.new("Liverpool Street")
-      end        
-          it "will be created with an entry station" do
-              expect(@journey.entry_station).to eq "Liverpool Street"
-          end
-        
+      
+      it "will be created with an entry station" do
+          expect(@journey.entry_station).to eq entry_station
+      end
+
+      it "knows if a journey is complete" do
+        @journey.finish(exit_station)
+        expect(@journey.complete?).to be true
+      end
+
+      it "returns itself when exiting a journey" do
+        expect(@journey.finish(exit_station)).to eq @journey
+      end
     end
 
-    describe '#fare' do
+      describe '#fare' do
 
-      it "has a penalty fare by default" do
-        expect(subject.fare).to eq Journey::PENALTY_FARE
-      end
+        it "has a penalty fare by default" do
+          expect(subject.fare).to eq Journey::PENALTY_FARE
+        end
 
-      it "will charge a penalty fare if journey's incomplete" do
-        @journey = Journey.new("Liverpool Street")
-        @journey.finish
-        expect(@journey.fare).to eq Journey::PENALTY_FARE
-      end
+        it "will charge a penalty fare if journey's incomplete" do
+          @journey.finish
+          expect(@journey.fare).to eq Journey::PENALTY_FARE
+        end
 
-      it "will charge £1 per complete journey" do
-        @journey = Journey.new("Liverpool Street")
-        @journey.finish("Walthamstow Central")
-        expect(@journey.fare).to eq 1
-      end
+        it "will charge £1 per complete journey" do
+          @journey.finish(exit_station)
+          expect(@journey.fare).to eq 1
+        end
     end
 end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,9 @@
 require 'oystercard'
 
 describe Oystercard do
+    let(:entry_station) { double :station }
+    let(:exit_station) { double :station }
+
     it "should have an initial default balance of 0" do
       expect(subject.balance).to eq 0
     end
@@ -24,33 +27,30 @@ describe Oystercard do
     end
 
     describe "#touch_in" do
-      let(:station) { double :station }
       context "when balance is equal or higher than minimum fare" do
         it "will change the state of the card for 'in use'" do
           subject.top_up(10)
-          subject.touch_in(station)
+          subject.touch_in(entry_station)
           expect(subject.in_jorney?).to be true
         end
 
         it "will save the entry station" do
           subject.top_up(10)
-          subject.touch_in("Liverpool Street")
+          subject.touch_in(entry_station)
           current_journey = subject.journeys[-1]
-          expect(current_journey.entry_station).to eq "Liverpool Street"
+          expect(current_journey.entry_station).to eq entry_station
         end
 
       end
 
       context "when balance is lower than minimum fare" do
         it "will raise an error" do
-          expect { subject.touch_in(station) }.to raise_error("Sorry, your card's balance isn't enough for travelling. Please top up.")
+          expect { subject.touch_in(entry_station) }.to raise_error("Sorry, your card's balance isn't enough for travelling. Please top up.")
         end
       end
     end
 
     describe "#touch_out" do
-      let(:entry_station) { double :station }
-      let(:exit_station) { double :station }
         context "in journey" do 
             before do
               subject.top_up(10)
@@ -67,9 +67,9 @@ describe Oystercard do
           end
 
           it "will save the exit station" do
-            subject.touch_out("Piccadilly Circus")
+            subject.touch_out(exit_station)
             current_journey = subject.journeys[-1]
-            expect(current_journey.exit_station).to eq "Piccadilly Circus"
+            expect(current_journey.exit_station).to eq exit_station
           end
         end
     end
